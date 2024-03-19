d94f09d82d0a4db4fbb3451461ae74aa903b4731
RubenHH019 <rhernandezh019@gmail.com>
2024-03-19 23:37:57 +0100

    Ejercicios resueltos

--- JAVACRIPT/Ejercicio 1/Iteracion6.js
+++ JAVACRIPT/Ejercicio 1/Iteracion6.js
@@ -1,12 +1,12 @@
 // 1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
 
-for (a = 0; a < 10; a++) {
+for (a = 0; a <= 9; a++) {
   console.log("1.1 El valor de a es:", a);
 }
 
 // 1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo
 // cuando el resto del numero dividido entre 2 sea 0.
-for (a = 0; a < 10; a++) {
+for (a = 0; a <= 9; a++) {
   if (a % 2 == 0) {
     console.log("1.2 Numeros que divididos entre 2 su resto sea 0:", a);
   }
@@ -15,39 +15,11 @@ for (a = 0; a < 10; a++) {
 // Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
 // Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle
 // y cambia el mensaje en la décima vuelta a 'Dormido!'.
-const dormir = "Vuelta 0";
-switch (dormir) {
-  case "Vuelta 0":
+
+for (a = 1; a <= 10; a++) {
+  if (a < 10) {
     console.log("Intentando dormir 🐑");
-    break;
-  case "Vuelta 1":
-    console.log("Intentando dormir 🐑");
-    break;
-  case "Vuelta 2":
-    console.log("Intentando dormir 🐑");
-    break;
-  case "Vuelta 3":
-    console.log("Intentando dormir 🐑");
-    break;
-  case "Vuelta 4":
-    console.log("Intentando dormir 🐑");
-    break;
-  case "Vuelta 5":
-    console.log("Intentando dormir 🐑");
-    break;
-  case "Vuelta 6":
-    console.log("Intentando dormir 🐑");
-    break;
-  case "Vuelta 7":
-    console.log("Intentando dormir 🐑");
-    break;
-  case "Vuelta 8":
-    console.log("Intentando dormir 🐑");
-    break;
-  case "Vuelta 9":
-    console.log("Intentando dormir 🐑");
-    break;
-  case "Vuelta 10":
+  } else {
     console.log("Dormido!");
-    break;
+  }
 }
