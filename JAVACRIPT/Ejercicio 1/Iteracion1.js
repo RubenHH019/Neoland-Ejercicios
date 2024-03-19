3505482445d54c914bfee361e6630033a5785e4f
RubenHH019 <rhernandezh019@gmail.com>
2024-03-19 15:13:56 +0100

    Practicas clase

--- JAVACRIPT/Ejercicio 1/Iteracion1.js
+++ JAVACRIPT/Ejercicio 1/Iteracion1.js
@@ -2,5 +2,13 @@
 let myFavoriteHero = "Hulk";
 console.log("Mi primer console.log:", myFavoriteHero);
 // 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+let x = 50;
+console.log("Valor variable x:", x);
 // 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
+let h = 5;
+let y = 10;
+console.log("Valor variable h:", h);
+console.log("Valor variable y:", y);
 // 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+let z = h + y;
+console.log("Valor variable z:", z);
