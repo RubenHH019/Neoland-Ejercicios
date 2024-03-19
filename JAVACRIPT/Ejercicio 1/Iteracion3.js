78206ae8e8211e394737439c0bebd608d9d2ebe0
RubenHH019 <rhernandezh019@gmail.com>
2024-03-19 16:10:48 +0100

    Practicas clase

---
+++ JAVACRIPT/Ejercicio 1/Iteracion3.js
@@ -0,0 +1,23 @@
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+const x = 10;
+a = x * 5;
+console.log("Resultado 1.1:", a);
+// 1.2 Divide 10 por 2 y muestra el resultado en un console.
+b = x / 2;
+console.log("Resultado 1.2:", b);
+// 1.3 Muestra mediante un console el resto de dividir 15 por 9.
+const y = 15;
+d = y % 9;
+console.log("Resultado 1.3:", d);
+// 1.4 Usa el correcto operador de asignación que resultará en o = 15,
+// teniendo dos variables p = 10 y j = 5.
+const p = 10;
+const j = 5;
+o = p + j;
+console.log("Resultado 1.4:", o);
+// 1.5 Usa el correcto operador de asignación que resultará en i = 50,
+// teniendo dos variables c = 10 y m = 5.
+const c = 10;
+const m = 5;
+i = c * m;
+console.log("Resultado 1.5:", i);
