c70d0ba9e2da9816d85b895578aa95bd2c261687
RubenHH019 <rhernandezh019@gmail.com>
2024-03-20 11:37:19 +0100

    Ejercicios resueltos

--- JAVACRIPT/Ejercicio 1/Iteracion3.js
+++ JAVACRIPT/Ejercicio 1/Iteracion3.js
@@ -1,23 +1,42 @@
 // 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
-const x = 10;
+let x = 10;
 a = x * 5;
-console.log("Resultado 1.1:", a);
+console.log("Resultado 1.1:", a); //? Forma lenta
+
+x *= 5;
+console.log("Resultado 1.1 rapido:", x); //TODO Forma rapida
+
 // 1.2 Divide 10 por 2 y muestra el resultado en un console.
 b = x / 2;
-console.log("Resultado 1.2:", b);
+console.log("Resultado 1.2:", b); //? Forma lenta
+
+x /= 2;
+console.log("Resultado 1.2 rapido:", x); //TODO Forma rapida
+
 // 1.3 Muestra mediante un console el resto de dividir 15 por 9.
-const y = 15;
+let y = 15;
 d = y % 9;
-console.log("Resultado 1.3:", d);
+console.log("Resultado 1.3:", d); //? Forma lenta
+
+y &= 9;
+console.log("Resultado 1.3 rapido:", y); //TODO Forma rapida
+
 // 1.4 Usa el correcto operador de asignación que resultará en o = 15,
 // teniendo dos variables p = 10 y j = 5.
-const p = 10;
-const j = 5;
+let p = 10;
+let j = 5;
 o = p + j;
-console.log("Resultado 1.4:", o);
+console.log("Resultado 1.4:", o); //? Forma lenta
+
+p += j;
+console.log("Resultado 1.4 rapido:", p); //TODO Forma rapida
+
 // 1.5 Usa el correcto operador de asignación que resultará en i = 50,
 // teniendo dos variables c = 10 y m = 5.
-const c = 10;
-const m = 5;
+let c = 10;
+let m = 5;
 i = c * m;
-console.log("Resultado 1.5:", i);
+console.log("Resultado 1.5:", i); //? Forma lenta
+
+c *= m;
+console.log("Resultado 1.5 rapido:", c); //TODO Forma rapida
