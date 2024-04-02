4077c6908c0d7d0d2a499f01d81f87652720c48d
RubenHH019 <rhernandezh019@gmail.com>
2024-04-02 17:29:27 +0200

    Funciones

--- JAVACRIPT/Ejercicio2/Iteracion8.js
+++ JAVACRIPT/Ejercicio2/Iteracion8.js
@@ -1,5 +1,7 @@
 //TODO Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma.
-
+let objeto = {};
+objeto.Clave = "Valor de la clave";
+console.log(objeto);
 const counterWords = [
   "code",
   "repeat",
@@ -13,6 +15,19 @@ const counterWords = [
   "upgrade",
   "code",
 ];
-function repeatCounter(param) {
-  // insert code
+
+function repeatCounter(contarRepetidas) {
+  let palabrasRepetidas = {};
+  contarRepetidas.forEach((palabra) => {
+    if (palabra in palabrasRepetidas) {
+      palabrasRepetidas[palabra] += 1;
+    } else {
+      palabrasRepetidas[palabra] = 1;
+    }
+    //  (palabra in palabrasRepetidas) ? palabrasRepetidas[palabra] += 1 : palabrasRepetidas[palabra] = 1;
+  });
+  return palabrasRepetidas;
 }
+
+let repetidas = repeatCounter(counterWords);
+console.log("🚀 ~  repetidas:", repetidas);
